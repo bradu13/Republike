1cfb04f5302df8c91c2ade8e70049d1677570dc4
Radu Ionut <ionut.radu@bytex.ro>
2020-04-21 15:24:58 +0300

    User Level Table

---
+++ src/db/models/userlevel.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const UserLevel = sequelize.define('UserLevel', {
+    name: DataTypes.STRING,
+    icon: DataTypes.STRING,
+    totalPoints: DataTypes.INTEGER
+  }, {});
+  UserLevel.associate = function(models) {
+    // associations can be defined here
+  };
+  return UserLevel;
+};
