45d557fbee612257771b4b71165bfb61e9bd8775
Radu Ionut <ionut.radu@bytex.ro>
2020-04-21 12:38:54 +0300

    Changed the id to uuid using the uuid-ossp extension

--- src/db/migrations/20200408103539-create-user.js
+++ src/db/migrations/20200408103539-create-user.js
@@ -1,78 +1,80 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true
-      },
-      password: {
-        type: Sequelize.STRING
-      },
-      fbToken: {
-        type: Sequelize.STRING
-      },
-      fullName: {
-        type: Sequelize.STRING
-      },
-      gender: {
-        type: Sequelize.INTEGER
-      },
-      birthDate: {
-        type: Sequelize.DATEONLY
-      },
-      country: {
-        type: Sequelize.STRING
-      },
-      biography: {
-        type: Sequelize.TEXT
-      },
-      aureusAmount: {
-        type: Sequelize.INTEGER,
-        defaultValue: 0
-      },
-      likeAmount: {
-        type: Sequelize.INTEGER,
-        defaultValue: 0
-      },
-      interestedIn: {
-        type: Sequelize.ARRAY(Sequelize.UUID)
-      },
-      favouritePosts: {
-        type: Sequelize.ARRAY(Sequelize.UUID)
-      },
-      friends: {
-        type: Sequelize.ARRAY(Sequelize.UUID)
-      },
-      friendRequests: {
-        type: Sequelize.ARRAY(Sequelize.UUID)
-      },
-      isActive: {
-        type: Sequelize.BOOLEAN,
-        allowNull: false,
-        defaultValue: false
-      },
-      isDeleted: {
-        type: Sequelize.BOOLEAN,
-        allowNull: false,
-        defaultValue: false
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
+    return queryInterface.sequelize.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
+      .then(() => {
+        return queryInterface.createTable('Users', {
+          id: {
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.literal('uuid_generate_v4()'),
+            primaryKey: true
+          },
+          email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true
+          },
+          password: {
+            type: Sequelize.STRING
+          },
+          fbToken: {
+            type: Sequelize.STRING
+          },
+          fullName: {
+            type: Sequelize.STRING
+          },
+          gender: {
+            type: Sequelize.INTEGER
+          },
+          birthDate: {
+            type: Sequelize.DATEONLY
+          },
+          country: {
+            type: Sequelize.STRING
+          },
+          biography: {
+            type: Sequelize.TEXT
+          },
+          aureusAmount: {
+            type: Sequelize.INTEGER,
+            defaultValue: 0
+          },
+          likeAmount: {
+            type: Sequelize.INTEGER,
+            defaultValue: 0
+          },
+          interestedIn: {
+            type: Sequelize.ARRAY(Sequelize.UUID)
+          },
+          favouritePosts: {
+            type: Sequelize.ARRAY(Sequelize.UUID)
+          },
+          friends: {
+            type: Sequelize.ARRAY(Sequelize.UUID)
+          },
+          friendRequests: {
+            type: Sequelize.ARRAY(Sequelize.UUID)
+          },
+          isActive: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+          },
+          isDeleted: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+          },
+          createdAt: {
+            allowNull: false,
+            type: Sequelize.DATE
+          },
+          updatedAt: {
+            allowNull: false,
+            type: Sequelize.DATE
+          }
+        });
+      });
   },
   down: (queryInterface) => {
     return queryInterface.dropTable('Users');
