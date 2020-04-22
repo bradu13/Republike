41a64a1d1cfb97c83e0fcf5c1d6c09da74e8af17
Radu Ionut-Alexandru <radui1398@gmail.com>
2020-04-22 14:47:02 +0300

    Add Notifications

--- src/db/models/user.model.js
+++ src/db/models/user.model.js
@@ -43,7 +43,7 @@ module.exports = (sequelize, DataTypes) => {
 
   User.associate = function (models) {
     models.User.hasMany(models.UserSetting);
-    models.User.hasmany(models.Notification);
+    models.User.hasMany(models.Notification);
   };
 
   return User;
