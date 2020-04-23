df416839fd978700d07c3fb3db031bdc693b59b1
Radu Ionut <ionut.radu@bytex.ro>
2020-04-23 10:12:21 +0300

    Refactor passport.js

--- src/config/passport.js
+++ src/config/passport.js
@@ -2,12 +2,12 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('../db').User;
 
-module.exports = function (passport) {
+module.exports = (passport) => {
   const opts = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('JWT'),
     secretOrKey: process.env.JWT
   };
-  passport.use('jwt', new JwtStrategy(opts, function (jwtPayload, done) {
+  passport.use('jwt', new JwtStrategy(opts, (jwtPayload, done) => {
     User
       .findByPk(jwtPayload.id)
       .then((user) => { return done(null, user); })
