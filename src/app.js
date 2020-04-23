e231a9d89a7411388c7be981136f83af65578b8f
Radu Ionut <ionut.radu@bytex.ro>
2020-04-23 10:05:19 +0300

    Refactor app.js

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+// Enable PassportJs
 require('./config/passport')(passport);
 
+// Load Routers
 const usersRouter = require('./routes/user.routes');
 const authRouter = require('./routes/auth.routes');
 const notificationRouter = require('./routes/notification.routes');
@@ -18,24 +20,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Config Routes
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
 app.use('/notifications', notificationRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
+// Catch 404 and forward to error handler
+app.use(function (req, res) {
   res.status(404).send(404);
 });
 
-// error handler
-app.use(function (err, req, res, next) {
+// Error handler
+app.use(function (err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // Send 500
+  res.status(err.status || 500).send('Internal server error');
 });
 
 module.exports = app;
