31a6a7f094006ca63eadde6241acfd9b57037cf5
Radu Ionut <ionut.radu@bytex.ro>
2020-04-24 10:10:57 +0300

    Refactor usersetting service

--- src/services/usersetting.services.js
+++ src/services/usersetting.services.js
@@ -1,15 +1,18 @@
 const UserSettingModel = require('../db').UserSetting;
 const strings = require('../util/strings');
 
-module.exports = {
-  getByUserId: async (id) => {
-    try {
-      return {
-        email: await UserSettingModel.findOne({ where: { UserId: id, type: 1 } }),
-        notification: await UserSettingModel.findOne({ where: { UserId: id, type: 2 } })
-      };
-    } catch (error) {
-      throw strings.errors.getUserSetting;
-    }
+// Get settings(notification and mail) by the UserId
+const getByUserId = async (id) => {
+  try {
+    return {
+      email: await UserSettingModel.findOne({ where: { UserId: id, type: 1 } }),
+      notification: await UserSettingModel.findOne({ where: { UserId: id, type: 2 } })
+    };
+  } catch (error) {
+    throw strings.errors.getUserSetting;
   }
+};
+
+module.exports = {
+  getByUserId
 };
