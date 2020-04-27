c954c0f4c43b7a8b0b0c48711e3ac42b778a6977
Radu Ionut <ionut.radu@bytex.ro>
2020-04-27 13:56:42 +0300

    Add Theme

---
+++ src/db/models/theme.model.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Theme = sequelize.define('Theme', {
+    name: DataTypes.STRING,
+    icon: DataTypes.STRING
+  }, {});
+  Theme.associate = function(models) {
+    // associations can be defined here
+  };
+  return Theme;
+};
