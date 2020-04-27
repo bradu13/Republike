cb73645dede06e7fe054cccfb0d2378729507d46
Radu Ionut <ionut.radu@bytex.ro>
2020-04-27 14:17:47 +0300

    Add Battle Schema

---
+++ src/db/schemas/battle.schemas.js
@@ -0,0 +1,36 @@
+const Joi = require('@hapi/joi');
+
+const update = Joi.object({
+  name: Joi.string().min(3).max(30),
+
+  description: Joi.string(),
+
+  type: Joi.number().min(1).max(4),
+
+  ThemeId: Joi.number(),
+
+  likeAmount: Joi.number(),
+
+  postAmount: Joi.number(),
+
+  shares: Joi.array()
+    .items(Joi.object({
+      UserId: Joi.uuid().required()
+    })),
+
+  views: Joi.array()
+    .items(Joi.object({
+      UserId: Joi.uuid().required()
+    })),
+});
+
+const create = update.keys({
+  name: Joi.string().min(3).max(30).required(),
+  type: Joi.number().min(1).max(4).required(),
+  ThemeId: Joi.number.required()
+});
+
+module.exports = {
+  update,
+  create
+};
